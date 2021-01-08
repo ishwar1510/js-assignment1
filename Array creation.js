45cff7a21e0c8f6cd8be93f726dff2918f48e40c
ishwar1510 <ijh.hkr9@gmail>
2021-01-08 19:24:51 +0530

    array creation

---
+++ Array creation.js
@@ -0,0 +1,7 @@
+// creation of an Array
+
+let fruits = ['Apple', 'Banana']
+
+console.log(fruits.length)
+
+//output is : 2
--- script.js
+++
@@ -1,6 +0,0 @@
-var stringArray = new Array("one", "two", "three", "four");
-
-for (var i = 0; i < stringArray.length ; i++) 
-{
-   console.log(stringArray[i]);
-}
