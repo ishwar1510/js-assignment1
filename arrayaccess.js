66d25d0fc9ca9543255c801bd8665910bd847b42
ishwar1510 <ijh.hkr9@gmail>
2021-01-08 22:28:38 +0530

    array of  5 types

--- arrayaccess.js
+++ arrayaccess.js
@@ -1,15 +1,33 @@
-//access single item from array
-var colors=['red','pink','orange','green','yellow']
-console.log(colors[0]);
+/*------Create an Array------*/
+
+//let colors=['red','pink','orange','green','yellow']
+//console.log(colors[0]);
+//console.log(colors[4]);
 
 
 
+/*----access last item from array in Javascript-----*/
+//let names=['lion','tiger','horse','monkey','elephant']
+//console.log(names[names.length-1]);
+
+
+
+/*-----access item from Array-----*/
 // var array = ['pink','red','green']
-
 // console. log(array[0])
 // console.log(array[1]);
 // console.log(array[2]);
-// console.log(array.length);
-// for(let i=0;i<=array.length-1;i++){
-//    console.log(array[i]);
-// }
+
+
+
+/*-----how to check lenght of the Array----*/
+//let array = ['karnataka','tailnadu','kerala','andrapradesh']
+ //console.log(array.length);
+ 
+
+
+ /*------access all items from Array----*/
+ let array = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+ for(let i=0;i<=array.length-1;i++){
+    console.log(array[i]);
+ }
