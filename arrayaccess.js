7d2f43757791a98a75f2ccd53369f287cc4ede8d
ishwar1510 <ijh.hkr9@gmail>
2021-01-08 20:31:06 +0530

    array

---
+++ arrayaccess.js
@@ -0,0 +1,15 @@
+//access single item from array
+var colors=['red','pink','orange','green','yellow']
+console.log(colors[0]);
+
+
+
+// var array = ['pink','red','green']
+
+// console. log(array[0])
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array.length);
+// for(let i=0;i<=array.length-1;i++){
+//    console.log(array[i]);
+// }
--- arraycreation.js
+++ arraycreation.js
@@ -5,3 +5,4 @@ let fruits = ['Apple', 'Banana']
 console.log(fruits.length)
 
 //output is : 2
+
