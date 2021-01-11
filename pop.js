9b67e96b299da15accad4c644fbbc89f38dc8d0b
ishwar1510 <ijh.hkr9@gmail>
2021-01-11 07:56:31 +0530

    pop

---
+++ pop.js
@@ -0,0 +1,12 @@
+/*---javascript array pop method --*/
+/* Definition and Usage
+The pop() method removes the last element of an array, and returns that element.
+
+Note: This method changes the length of an array. */
+
+
+let array = [1,'ishwar',25,'prostack',55,'bangalore'];
+array.pop('bangalore'); //removing string item
+console.log(array);
+array.pop(25); //removing number from array.
+console.log(array);
