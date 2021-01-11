60dba9b03a84de9dc04646819a6e7d463662609a
ishwar1510 <ijh.hkr9@gmail>
2021-01-11 07:50:09 +0530

    push item into array

---
+++ push.js
@@ -0,0 +1,11 @@
+/*---javascript Array push method --*/
+/*Definition and Usage
+The push() method adds new items to the end of an array, and returns the new length.
+
+Note: The new item(s) will be added at the end of the array.
+
+Note: This method changes the length of the array.*/
+
+let names = ['ishwar','sattya','ravi','prostack','pradeep'];
+names.push('narasimha reddy');
+console.log(names);
