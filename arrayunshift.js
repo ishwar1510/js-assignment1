694af73c41a230b4db559fd62c3e34221f63fec1
ishwar1510 <ijh.hkr9@gmail>
2021-01-11 08:07:39 +0530

    array unshift

---
+++ arrayunshift.js
@@ -0,0 +1,10 @@
+/*--- javascript array unshift method --*/
+
+/* Definition and Usage
+The unshift() method adds new items to the beginning of an array, and returns the new length.
+
+Note: This method changes the length of an array. */
+
+let x = [20,54,36,59];
+x.unshift('ishwar','prostack');
+console.log(x);
