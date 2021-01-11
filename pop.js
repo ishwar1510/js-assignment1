1206a508cba693c479040a06a46d3dd85275584f
ishwar1510 <ijh.hkr9@gmail>
2021-01-11 08:00:13 +0530

    pop

--- pop.js
+++ pop.js
@@ -6,7 +6,7 @@ Note: This method changes the length of an array. */
 
 
 let array = [1,'ishwar',25,'prostack',55,'bangalore'];
-array.pop('bangalore'); //removing string item
+array.pop(); //removing last item from the array
 console.log(array);
 array.pop(25); //removing number from array.
 console.log(array);
