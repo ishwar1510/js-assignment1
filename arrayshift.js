7c7b638527a915b07b17e2db4b3126d85ad1fddb
ishwar1510 <ijh.hkr9@gmail>
2021-01-11 08:04:01 +0530

    shift

---
+++ arrayshift.js
@@ -0,0 +1,12 @@
+/*---javascript array shift method ---*/
+
+/* Definition and Usage
+The shift() method removes the first item of an array.
+
+Note: This method changes the length of the array.
+
+Note: The return value of the shift method is the removed item. */
+
+let array = ['pink',23,56,80,90];
+array.shift();
+console.log(array);
