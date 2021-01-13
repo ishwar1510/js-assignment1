df5c389a1b035f4e4b3c4a47f9bff2bdb5a1dbea
ishwar1510 <ijh.hkr9@gmail>
2021-01-13 21:11:41 +0530

    date printing

---
+++ one.js
@@ -0,0 +1,10 @@
+var d = new Date();
+// let x=d.getFullYear() //year as a four digit unit (yyyy)
+// let x=d.getMonth() //diplay month as a number (0-11)
+// let x=d.getHours() //get the hour (0-23)
+// let x=d.getDate(); //day as a number (0-31)
+// let x=d.getMinutes() //get the minute (0-59)
+// let x=d.getSeconds(); //get the second (0-59)
+// let x=d.getMilliseconds(); //get the millisecond (0-999)
+let x=d.getTime(); //get the time (milliseconds since january 1, 1970)
+console.log(x);
