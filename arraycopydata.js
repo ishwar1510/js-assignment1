673f9157015b0d844b28bee964f646a8a4dddc88
ishwar1510 <ijh.hkr9@gmail>
2021-01-13 17:19:22 +0530

    array copy

---
+++ arraycopydata.js
@@ -0,0 +1,14 @@
+/*---copy the data from array in js---*/
+let oldArray = [1, 2, 3, 4, 5];
+
+let newArray = oldArray.slice(2,3);
+
+console.log(newArray);
+
+
+// let x = [1, 2, 3];
+// let y = Array.from(x);
+
+// console.log(y);
+
+
